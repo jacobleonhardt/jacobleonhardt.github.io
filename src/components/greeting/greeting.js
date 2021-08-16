8fdabf99d47e8ae160f66577a4424500eca79a08
leonhardt <jacob.leonhardt649@gmail.com>
2021-08-16 11:41:26 -0500

    update resume

--- src/components/greeting/greeting.js
+++ src/components/greeting/greeting.js
@@ -7,7 +7,7 @@ import './greeting.css'
 const Greeting = () => {
 
     const viewResume = () => {
-        window.open("https://www.linkedin.com/in/jacob-leonhardt-b19067ba/", '_blank')
+        window.open("https://drive.google.com/file/d/1gHM7VMonOlTFv0yIvumT2QWTZaScCdZO/view?usp=sharing", '_blank')
     }
 
     return (
@@ -15,8 +15,8 @@ const Greeting = () => {
             <img src={leonhardtLogo} className="logo" alt="Jacob Leonhardt logo" />
             <h1>Jacob Leonhardt</h1>
             <h2>A Creative Full-Stack Developer</h2>
-            <Link className="button alt" onClick={viewResume}>Resume</Link>
-            <Link id='arrow' className="icon-link" to="skills" smooth={true} spy={true} duration={800} activeClass="active"><ion-icon name="chevron-down-outline"></ion-icon></Link>
+            <Link id="resume" className="button alt" onClick={viewResume}>Resume</Link>
+            <Link id="arrow" className="icon-link" to="skills" smooth={true} spy={true} duration={800} activeClass="active"><ion-icon name="chevron-down-outline"></ion-icon></Link>
         </div>
     )
 }
