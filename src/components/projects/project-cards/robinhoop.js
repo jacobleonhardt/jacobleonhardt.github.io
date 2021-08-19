f22bb97ab4bd5989566b18f3d736a970b140ffc1
leonhardt <jacob.leonhardt649@gmail.com>
2021-08-19 17:07:57 -0500

    adjusting Robinhoop copy

--- src/components/projects/project-cards/robinhoop.js
+++ src/components/projects/project-cards/robinhoop.js
@@ -12,8 +12,8 @@ const Robinhoop = () => {
                 great group of guys to design and construct the app. It utilizes
                 Python and Flask in the backend, and React and Redux in the
                 front. This project ended up being more interesting than I
-                initially suspected. While I'm not a basketball fan, it was a
-                good experience to work on a project that others could get in to.</p>
+                initially suspected. While I'm not a basketball fan, it was good
+                experience to work on a project that others could get in to.</p>
             <a href="https://robinhoop.herokuapp.com/" target="_blank" className="button">Checkout Robinhoop</a>
             <a href="https://github.com/NCHitsman/Python-Group-Project-Team-7" target="_blank" className="button">GitHub Repo</a>
         </div>
