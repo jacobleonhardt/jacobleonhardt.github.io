52ea935d89548981fecffa564da5efbf320a24fb
Jacob Leonhardt <jacobleonhardt@Jacobs-MacBook-Pro-2.local>
2022-05-11 17:47:05 -0500

    update resume

--- src/components/greeting/greeting.js
+++ src/components/greeting/greeting.js
@@ -6,14 +6,14 @@ import './greeting.css'
 const Greeting = () => {
 
     const viewResume = () => {
-        window.open("https://drive.google.com/file/d/1_qGKw1HOGLgZMHOTBkznY_AlHgHOgXmt/view?usp=sharing", '_blank')
+        window.open("https://drive.google.com/file/d/1MpckKjrr3fFTRbvIMRbvA-MVku0-YrLt/view?usp=sharing", '_blank')
     }
 
     return (
         <div id="greeting" className="card">
             <img src={leonhardtLogo} className="logo" alt="Jacob Leonhardt logo" />
             <h1>Jacob Leonhardt</h1>
-            <h2>A Creative Full-Stack Developer</h2>
+            <h2>A Creative Frontend Developer</h2>
             <Link id="resume" className="button alt" onClick={viewResume}>Resume</Link>
             <Link id="arrow" className="icon-link" to="skills" smooth={true} spy={true} duration={800} activeClass="active"><ion-icon name="chevron-down-outline"></ion-icon></Link>
         </div>
