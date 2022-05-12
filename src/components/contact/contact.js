6c79712eb818128effb959fb03ad181e4cea0d3c
leonhardt <jacob.leonhardt649@gmail.com>
2022-05-11 21:59:39 -0500

    update phone icon in contact section

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -10,7 +10,7 @@ const Contact = () => {
                 <a href="https://github.com/jacobleonhardt" target="_blank"><ion-icon name="logo-github"></ion-icon></a>
                 <a href="https://www.linkedin.com/in/jacob-leonhardt-b19067ba/" target="_blank"><ion-icon name="logo-linkedin"></ion-icon></a>
                 <a href="mailto:jacob.leonhardt649@gmail.com" target="_blank"><ion-icon name="mail-outline"></ion-icon></a>
-                <a href="tel:+12707918120" target="_blank">  <ion-icon name="phone-portrait"></ion-icon></a>
+                <a href="tel:+12707918120" target="_blank"><ion-icon name="call-outline"></ion-icon></a>
             </div>
         </div>
     )
