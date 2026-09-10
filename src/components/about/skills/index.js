7dd8f809c734f54d5669a07279f7e9ca55c0ad57
Jacob Leonhardt <jacob.leonhardt649@gmail.com>
2026-09-10 07:44:16 -0500

    Update index.js

--- src/components/about/skills/index.js
+++ src/components/about/skills/index.js
@@ -7,17 +7,11 @@ const Skills = () => {
         <div id="skills" className="full">
             <h2>Skills</h2>
             <div id="additional">
-                <i class="devicon-ruby-plain colored"></i>
                 <i class="devicon-javascript-plain colored"></i>
                 <i class="devicon-python-plain colored"></i>
                 <i class="devicon-html5-plain colored"></i>
                 <i class="devicon-css3-plain colored"></i>
-                <i class="devicon-rails-plain colored"></i>
                 <i class="devicon-react-original colored"></i>
-                <i class="devicon-redux-original colored"></i>
-                <i class="devicon-rspec-original colored"></i>
-                <i class="devicon-sqlalchemy-plain"></i>
-                <i class="devicon-sequelize-plain"></i>
                 <i class="devicon-postgresql-plain"></i>
                 <i class="devicon-git-plain colored"></i>
                 <i class="devicon-github-original colored"></i>
