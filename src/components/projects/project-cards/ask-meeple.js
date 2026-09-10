1bbb8a16c00c353ae48fb7d9feddf5bfe3d25c30
Jacob Leonhardt <jacob.leonhardt649@gmail.com>
2026-09-10 07:50:46 -0500

    Remove live site link from Ask Meeple component
    Removed the link to the Ask Meeple live site from the project cards component.

--- src/components/projects/project-cards/ask-meeple.js
+++ src/components/projects/project-cards/ask-meeple.js
@@ -11,7 +11,6 @@ const AskMeeple = () => {
                 to work with two other fantastic developers. I headed up the
                 frontend design on the project. For this one we used Pug as our
                 structure code, and custom CSS for design.</p>
-            <a href="https://ask-meeple.herokuapp.com/" target="_blank" className="button">Checkout Ask Meeple</a>
             <a href="https://github.com/TranquilTort/Ask-Meeple" target="_blank" className="button">GitHub Repo</a>
         </div>
     )
